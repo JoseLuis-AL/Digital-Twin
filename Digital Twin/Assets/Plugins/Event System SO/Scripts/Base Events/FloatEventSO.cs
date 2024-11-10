7a37ed01bdc53c05f6547319ea2fcdd6a9d5d47f
Jose Luis Aguilera Luzania <The3rPlayer@outlook.com>
2024-11-10 04:55:48 -0700

    Init

--- Digital Twin/Assets/Plugins/Event System SO/Scripts/Base Events/FloatEventSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Plugins.Event_System_SO.Scripts.Base_Events
-{
-    [CreateAssetMenu(menuName = "Events SO/Float Event")]
-    public class FloatEventSO : EventSO<float>
-    {
-    }
-}
